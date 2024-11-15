2ec6c2110b15fdb68db4d845a6550e948bcba31b
Francisco <castanho.jfc@gmail.com>
2024-11-15 01:38:56 +0000

    chore(recipes): add model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_15_004323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.integer "cook_time"
+    t.integer "prep_time"
+    t.string "ingredients", default: [], array: true
+    t.decimal "ratings"
+    t.string "cuisine"
+    t.string "category"
+    t.string "author"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
