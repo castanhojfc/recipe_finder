7c4c184c63b25e84ba73d8714e584eb3406dd166
Francisco <castanho.jfc@gmail.com>
2024-11-15 01:39:46 +0000

    test(recipes): cover simple endpoint

---
+++ test/controllers/recipes_controller_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class RecipesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @recipe = recipes(:one)
+  end
+
+  test "should get index" do
+    get recipes_url
+    assert_response :success
+  end
+end
