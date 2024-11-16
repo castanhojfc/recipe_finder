1a58880f86e28bc12dc4a22b6abeb6642e7f4e70
Francisco <castanho.jfc@gmail.com>
2024-11-16 20:34:36 +0000

    chore(migrations): use trigger to recalculate tsvector

--- db/migrate/20241115163613_add_search_field_to_recipes.rb
+++ db/migrate/20241115163613_add_search_field_to_recipes.rb
@@ -2,21 +2,30 @@ class AddSearchFieldToRecipes < ActiveRecord::Migration[8.0]
   def up
     add_column :recipes, :searchable_lexemes, :tsvector
 
-    reversible do |dir|
-      dir.up do
-        execute <<-SQL
-          UPDATE recipes
-          SET searchable_lexemes = (
-            setweight(to_tsvector('english', coalesce(title, '')), 'A') ||
-            setweight(to_tsvector('english', coalesce(category, '')), 'B') ||
-            setweight(to_tsvector('english', coalesce(array_to_string(ingredients, ' '), '')), 'C')
-          );
-        SQL
-      end
-    end
+    execute <<-SQL
+      CREATE FUNCTION update_searchable_lexemes()
+        RETURNS trigger AS $$
+        BEGIN
+          NEW.searchable_lexemes := setweight(to_tsvector('english', coalesce(NEW.title, '')), 'A') ||
+                                  setweight(to_tsvector('english', coalesce(NEW.category, '')), 'B') ||
+                                  setweight(to_tsvector('english', coalesce(array_to_string(NEW.ingredients, ' '), '')), 'C');
+          RETURN NEW;
+        END;
+      $$ LANGUAGE plpgsql;
+
+      CREATE TRIGGER update_searchable_lexemes_trigger
+      BEFORE INSERT OR UPDATE ON recipes
+      FOR EACH ROW
+      EXECUTE FUNCTION update_searchable_lexemes();
+    SQL
   end
 
   def down
     remove_column :recipes, :searchable_lexemes
+
+    execute <<-SQL
+      DROP TRIGGER IF EXISTS update_searchable_lexemes_trigger ON recipes;
+      DROP FUNCTION IF EXISTS update_searchable_lexemes;
+    SQL
   end
 end
