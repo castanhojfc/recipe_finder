73782d2fb66f75d4b5804f01ae3e2a04649963a4
Francisco <castanho.jfc@gmail.com>
2024-11-17 20:41:59 +0000

    test(models): cover recipe search

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @golden_sweet_cornbread = recipes(:golden_sweet_cornbread)
+    @monkey_bread_i = recipes(:monkey_bread_i)
+  end
+
+  test "search by title returns correct recipe" do
+    query = "Golden Sweet Cornbread"
+    result = Recipe.search(query)
+
+    assert_includes result, @golden_sweet_cornbread
+    assert_not_includes result, @monkey_bread_i
+  end
 end
