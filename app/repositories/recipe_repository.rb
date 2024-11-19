814af631f66d347cae10c2588b735232600138f8
Francisco <castanho.jfc@gmail.com>
2024-11-19 19:35:22 +0000

    chore(recipes): cleanup

--- app/repositories/recipe_repository.rb
+++ app/repositories/recipe_repository.rb
@@ -2,17 +2,12 @@ module Repositories
   class RecipeRepository
     def self.search(query, column, direction)
       if query.present?
-        if column.present?
-          Recipe.search_and_sort(query, column, direction)
-        else
-          Recipe.search(query)
-        end
+        column.present? ? Recipe.search_and_sort(query, column, direction) : Recipe.search(query)
       else
-        if column.present?
-          Recipe.all.limit(Pagy::DEFAULT[:items]).order("#{column} #{direction}")
-        else
-          Recipe.all.limit(Pagy::DEFAULT[:items])
-        end
+        scope = Recipe.all
+        scope = scope.order("#{column} #{direction}") if column.present?
+
+        scope.limit(Pagy::DEFAULT[:items])
       end
     end
   end
--- app/views/recipes/_recipe.jbuilder
+++
@@ -1,8 +0,0 @@
-json.title recipe.title
-json.category recipe.category
-json.ingredients recipe.ingredients
-json.cook_time recipe.cook_time
-json.prep_time recipe.prep_time
-json.ratings recipe.ratings
-json.author recipe.author
-json.image recipe.image
