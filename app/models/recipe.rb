cfbf00acb7453da420f12f0aa5d0a5dff2009d97
Francisco <castanho.jfc@gmail.com>
2025-03-09 20:30:19 +0000

    chore(recipe_repository): build scope dynamically

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,14 +8,4 @@ class Recipe < ApplicationRecord
                     dictionary: "english", tsvector_column: "searchable_lexemes"
                     }
                   }
-
-  def self.search_and_sort(query, sort_column, sort_direction)
-    allowed_columns = %w[title category number_of_ingredients cook_time prep_time ratings author]
-    allowed_directions = %w[asc desc]
-
-    sort_column = allowed_columns.include?(sort_column) ? sort_column : "ratings"
-    sort_direction = allowed_directions.include?(sort_direction.downcase) ? sort_direction : "desc"
-
-    search(query).reorder("#{sort_column} #{sort_direction}")
-  end
 end
--- app/repositories/recipe_repository.rb
+++ app/repositories/recipe_repository.rb
@@ -2,13 +2,26 @@ module Repositories
   class RecipeRepository
     def self.search(query, column, direction)
       if query.present?
-        column.present? ? Recipe.search_and_sort(query, column, direction) : Recipe.search(query)
+        scope = Recipe.search(query)
+
+        if sorting_params_present?(column, direction)
+          scope = scope.reorder("#{column} #{direction}")
+        end
       else
         scope = Recipe.all
-        scope = scope.order("#{column} #{direction}") if column.present?
 
-        scope.limit(Pagy::DEFAULT[:items])
+        if sorting_params_present?(column, direction)
+          scope = scope.order("#{column} #{direction}")
+        end
       end
+
+      scope.limit(Pagy::DEFAULT[:items])
+    end
+
+    private
+
+    def self.sorting_params_present?(column, direction)
+      column.present? and direction.present?
     end
   end
 end
